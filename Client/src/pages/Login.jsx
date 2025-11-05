cd15fb06c77f4317f14b22c5cbb1e4a98f18b96c
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-05 13:45:47 +0530

    Clerk Auth Setup

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { Star } from "lucide-react";
+import { SignIn } from "@clerk/clerk-react";
 
 const Login = () => {
   return (
@@ -49,7 +50,9 @@ const Login = () => {
         <span className="md:h-10"></span>
       </div>
       {/* ---------------- RIGHT - LOGIN FORM ---------------- */}
-      <div className="flex-1 flex items-center justify-center p-6 sm:p-10"></div>
+      <div className="flex-1 flex items-center justify-center p-6 sm:p-10">
+        <SignIn />
+      </div>
     </div>
   );
 };
