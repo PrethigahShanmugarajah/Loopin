b48d1be2ab1bdda16f67de96019848257aa6d80d
PrethigahShanmugarajah <prethigah@gmail.com>
2025-11-06 01:39:41 +0530

    Created Discover page

--- Client/src/pages/Discover.jsx
+++ Client/src/pages/Discover.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { dummyConnectionsData } from "../assets/assets";
+import { Search } from "lucide-react";
+import UserCard from "../components/UserCard";
+import Loading from "../components/Loading";
 
 const Discover = () => {
+  const [input, setInput] = useState("");
+  const [users, setUsers] = useState(dummyConnectionsData);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      setUsers([]);
+      setLoading(true);
+      setTimeout(() => {
+        setUsers(dummyConnectionsData);
+        setLoading(false);
+      }, 1000);
+    }
+  };
   return (
-    <div className="min-h-screen flex items-center justify-center bg-orange-100">
-      <h1 className="text-4xl font-bold text-orange-700">Discover</h1>
+    <div className="min-h-screen bg-linear-to-b from-slate-50 to-white">
+      <div className="max-w-6xl mx-auto p-6">
+        {/* ---------------- TITLE ---------------- */}
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">
+            Discover People
+          </h1>
+          <p className="text-slate-600">
+            Find new friends and expand your circle with inspiring people.
+          </p>
+        </div>
+
+        {/* ---------------- SEARCH ---------------- */}
+        <div className="mb-8 shadow-md rounded-md border border-slate-200/60 bg-white/80">
+          <div className="p-6">
+            <div className="relative">
+              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400 w-5 h-5" />
+              <input
+                type="text"
+                placeholder="Search people by name, username, bio, or location..."
+                className="pl-10 sm:pl-12 py-2 w-full border border-gray-300 rounded-md max-sm:text-sm"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                onKeyUp={handleSearch}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap gap-6">
+          {users.map((user) => (
+            <UserCard user={user} key={user._id} />
+          ))}
+        </div>
+
+        {loading && <Loading height="60vh" />}
+      </div>
     </div>
   );
 };
