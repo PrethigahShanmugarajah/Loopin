9a14291fe1b1a9a79735287a5fceed40856e6b30
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-18 21:21:12 +0530

    Create function for Get User Data Using UserId

---
+++ Server/controllers/userController.js
@@ -0,0 +1,23 @@
+// Server / controllers / userController.js
+import User from "../models/User.js";
+
+/* -------- Get User Data Using UserId -------- */
+export const getUserData = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.json({ success: false, message: "User not found!" });
+    }
+    res.json({ success: true, user });
+  } catch (error) {
+    console.error("Get User Data Using UserId Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: `Get User Data Using UserId Error: ${
+        error.code || error.message
+      }`,
+    });
+  }
+};
