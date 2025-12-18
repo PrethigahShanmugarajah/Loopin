6d0b5e68f3d897b8c1b2e3bec7c341d11438840d
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-19 00:06:58 +0530

    Create function for Send Connection Request

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,5 +1,6 @@
 // Server / controllers / userController.js
 import imagekit from "../configs/imageKit.js";
+import Connection from "../models/Connection.js";
 import User from "../models/User.js";
 import fs from "fs";
 
@@ -205,6 +206,66 @@ export const unfollowUser = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: `Unfollow User Error: ${error.code || error.message}`,
+    });
+  }
+};
+
+/* -------- Send Connection Request -------- */
+export const sendConnectionRequest = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+
+    // Check if user has sent more than 20 connection requests in the last 24 hours
+    const last24Hours = new Date(Date.now() - 24 * 60 * 60 * 1000);
+    const connectionRequests = await Connection.find({
+      from_user_id: userId,
+      createdAt: { $gt: last24Hours },
+    });
+
+    if (connectionRequests.length >= 20) {
+      return res.status(429).json({
+        success: false,
+        message:
+          "You have sent more than 20 connection requests in the last 24 hours",
+      });
+    }
+
+    // Check if users are already connected
+    const connection = await Connection.findOne({
+      $or: [
+        { rom_user_id: userId, to_user_id: id },
+        { rom_user_id: id, to_user_id: userId },
+      ],
+    });
+
+    if (!connection) {
+      await Connection.create({
+        from_user_id: userId,
+        to_user_id: id,
+      });
+
+      return res.status(201).json({
+        success: true,
+        message: "Connection request sent successfully!",
+      });
+    } else if (connection && connection.status === "accepted") {
+      return res.status(200).json({
+        success: false,
+        message: "You are already connected with this user",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Connection request pending!",
+    });
+  } catch (error) {
+    console.error("Send Connection Request Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: `Send Connection Request Error: ${error.code || error.message}`,
     });
   }
 };
