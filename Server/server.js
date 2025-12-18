b64910a796a48f98f8710f3b1cbec2b4f19c2f31
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-18 18:44:55 +0530

    Set up Inngest routes

--- Server/server.js
+++ Server/server.js
@@ -3,16 +3,24 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
+/* -------- INITIALIZE EXPRESS -------- */
 const app = express();
 
+/* -------- CONNECT TO DATABASE -------- */
 await connectDB();
 
+/* -------- MIDDLEWARE CONFIGURATION -------- */
 app.use(express.json());
 app.use(cors());
 
+/* -------- ROUTES -------- */
 app.get("/", (req, res) => res.send("Server is running"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
+/* -------- PORT -------- */
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
