baed3116f6af9821879e930aca28a0d043d7d599
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-18 21:21:24 +0530

    Add userRoutes

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,7 @@ import connectDB from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
+import userRouter from "./routes/userRoutes.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -21,8 +22,11 @@ app.use(clerkMiddleware());
 /* -------- ROUTES -------- */
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/user", userRouter);
 
 /* -------- PORT -------- */
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
