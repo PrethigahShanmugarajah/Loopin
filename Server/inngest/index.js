a1e7b984353985511216421e987d45d72601ba04
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-19 01:30:41 +0530

    Add Inngest function to send reminder for new connection request

---
+++ Server/inngest/index.js
@@ -0,0 +1,171 @@
+// Server/inngest/index.js
+import { Inngest } from "inngest";
+import User from "../models/User.js";
+import Connection from "../models/Connection.js";
+import sendEmail from "../configs/nodeMailer.js";
+
+/* -------- CREATE A CLIENT TO SEND AND RECEIVE EVENTS -------- */
+export const inngest = new Inngest({ id: "loopin-app" });
+
+/* -------- INNGEST FUNCTION TO SAVE USER DATA TO A DATABASE -------- */
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    let username = email_addresses[0].email_address.split("@")[0];
+
+    /* ---- CHECK AVAILABILITY OF USERNAME ---- */
+    const user = await User.findOne({ username });
+
+    if (user) {
+      username = username + Math.floor(Math.random() * 10000);
+    }
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      full_name: first_name + " " + last_name,
+      profile_picture: image_url,
+      username,
+    };
+
+    await User.create(userData);
+  }
+);
+
+/* -------- INNGEST FUNCTION TO UPDATE USER DATA TO A DATABASE -------- */
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    const updatedUserData = {
+      email: email_addresses[0].email_address,
+      full_name: first_name + " " + last_name,
+      profile_picture: image_url,
+    };
+    await User.findByIdAndUpdate(id, updatedUserData);
+  }
+);
+
+/* -------- INNGEST FUNCTION TO DELETE USER DATA TO A DATABASE -------- */
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await User.findByIdAndDelete(id);
+  }
+);
+
+/* -------- INNGEST FUNCTION TO SEND REMINDER WHEN A NEW CONNECTION REQUEST IS ADDED -------- */
+const sendNewConnectionRequestReminder = inngest.createFunction(
+  { id: "send-new-connection-request-reminder" },
+  { event: "app/connection-request" },
+  async ({ event, step }) => {
+    const { connectionId } = event.data;
+
+    await step.run("send-connection-request-mail", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id"
+      );
+
+      const subject = `🤝 New Connection Request`;
+      const body = `<div style="font-family: Inria Serif, serif; padding: 20px;">
+          <h2>Hi ${connection.to_user_id.full_name},</h2>
+          <p>
+            You have a new connection request from
+            <strong>
+              ${connection.from_user_id.full_name} - @$
+              {connection.from_user_id.username}
+            </strong>
+          </p>
+
+          <p>
+            Click
+            <a
+              href="${process.env.FRONTEND_URL}/connections"
+              style="color: #F97316;"
+            >
+              here
+            </a>
+            to accept or reject the request.
+          </p>
+
+          <br />
+
+          <p>
+            Thanks, <br />Loopin - Stay Connected
+          </p>
+        </div>`;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+    });
+
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    await step.sleepUntil("wait-for-24-hours", in24Hours);
+    await step.run("send=connection-request-reminder", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id"
+      );
+
+      if (connection.status === "accepted") {
+        return { message: "Already accepted" };
+      }
+
+      const subject = `🤝 New Connection Request`;
+      const body = `<div style="font-family: Inria Serif, serif; padding: 20px;">
+          <h2>Hi ${connection.to_user_id.full_name},</h2>
+          <p>
+            You have a new connection request from
+            <strong>
+              ${connection.from_user_id.full_name} - @$
+              {connection.from_user_id.username}
+            </strong>
+          </p>
+
+          <p>
+            Click
+            <a
+              href="${process.env.FRONTEND_URL}/connections"
+              style="color: #F97316;"
+            >
+              here
+            </a>
+            to accept or reject the request.
+          </p>
+
+          <br />
+
+          <p>
+            Thanks, <br />Loopin - Stay Connected
+          </p>
+        </div>`;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+
+      return { message: "Reminder sent successfully!" };
+    });
+  }
+);
+
+/* -------- CREATE AN EMPTY ARRAY WHERE WE'LL EXPORT FUTURE INNGEST FUNCTIONS -------- */
+export const functions = [
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+  sendNewConnectionRequestReminder,
+];
