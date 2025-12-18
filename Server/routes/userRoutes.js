870b26b709020705c11868ea64446ccc7dd6d617
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-18 22:45:48 +0530

    Create routes for updateUserData

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,10 +1,20 @@
 // Server / routes / userRoutes.js
 import express from "express";
 import { protect } from "../middlewares/auth.js";
-import { getUserData } from "../controllers/userController.js";
+import { getUserData, updateUserData } from "../controllers/userController.js";
+import { upload } from "../configs/multer.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/data", protect, getUserData);
+userRouter.post(
+  "/update",
+  upload.fields([
+    { name: "profile", maxCount: 1 },
+    { name: "cover", maxCount: 1 },
+  ]),
+  protect,
+  updateUserData
+);
 
 export default userRouter;
