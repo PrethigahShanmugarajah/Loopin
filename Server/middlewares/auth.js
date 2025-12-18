64a1c528bb1ef5b0975406af0805abb31001fec1
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-18 20:27:26 +0530

    Add protect middleware for authentication

---
+++ Server/middlewares/auth.js
@@ -0,0 +1,23 @@
+// Server / middlewares / auth.js
+export const protect = async (req, res, next) => {
+  try {
+    const { userId } = await req.auth();
+
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not Authenticated" });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Protect middleware Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: `Protect middleware Error: ${
+        error.code || error.message || "Internal Server Error"
+      }`,
+    });
+  }
+};
