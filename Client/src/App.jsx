1fcc2e8435a3c29ed0529c3361b5e58712aad6e1
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-20 21:46:16 +0530

    Dispatch fetchUser action with token from Clerk useAuth

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,3 +1,4 @@
+// Client / src / App.jsx
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
@@ -11,16 +12,25 @@ import { useAuth, useUser } from "@clerk/clerk-react";
 import Layout from "./pages/Layout";
 import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "./features/user/userSlice";
 
 const App = () => {
   const { user } = useUser();
   const { getToken } = useAuth();
 
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    if (user) {
-      getToken().then((token) => console.log("Token:", token));
-    }
-  }, [user]);
+    const fetchData = async () => {
+      if (user) {
+        const token = await getToken();
+        console.log("Token:", token);
+        dispatch(fetchUser(token));
+      }
+    };
+    fetchData();
+  }, [user, getToken, dispatch]);
 
   return (
     <>
