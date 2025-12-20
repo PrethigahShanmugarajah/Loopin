26849c2695b465236ddcfdca381c578f3772e149
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-20 18:39:26 +0530

    Create routes for sendMessage

--- Server/routes/messageRoutes.js
+++ Server/routes/messageRoutes.js
@@ -1,8 +1,14 @@
 // Server / routes / messageRoutes.js
 import express from "express";
-import { sseController } from "../controllers/messageController.js";
+import {
+  sendMessage,
+  sseController,
+} from "../controllers/messageController.js";
+import { upload } from "../configs/multer.js";
+import { protect } from "../middlewares/auth.js";
 
 const messageRouter = express.Router();
 messageRouter.get("/:userId", sseController);
+messageRouter.post("/send", upload.single("image"), protect, sendMessage);
 
 export default messageRouter;
