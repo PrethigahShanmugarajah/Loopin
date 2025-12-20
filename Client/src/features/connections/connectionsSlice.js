e7547f142fbbebd061fb5da015678a806a0237c4
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-20 21:59:50 +0530

    Create Redux slice for user connections state

---
+++ Client/src/features/connections/connectionsSlice.js
@@ -0,0 +1,17 @@
+// Client / src / features / connections / connectionsSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  connections: [],
+  pendingConnections: [],
+  followers: [],
+  following: [],
+};
+
+const connectionsSlice = createSlice({
+  name: "connections",
+  initialState,
+  reducers: {},
+});
+
+export default connectionsSlice.reducer;
