ce13db8abe8388c0e561f21896e16be29f7713a6
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-20 19:04:38 +0530

    Create routes for getUserRecentMessages

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -13,6 +13,7 @@ import {
   updateUserData,
 } from "../controllers/userController.js";
 import { upload } from "../configs/multer.js";
+import { getUserRecentMessages } from "../controllers/messageController.js";
 
 const userRouter = express.Router();
 
@@ -33,5 +34,6 @@ userRouter.post("/connect", protect, sendConnectionRequest);
 userRouter.post("/accept", protect, acceptConnectionRequest);
 userRouter.get("/connections", protect, getUserConnections);
 userRouter.post("/profile", getUserProfiles);
+userRouter.get("/recent-messages", protect, getUserRecentMessages);
 
 export default userRouter;
