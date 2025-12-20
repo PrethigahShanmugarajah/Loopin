d456129782c4e216f3020c0985cc4694e40cf5e1
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-20 18:53:31 +0530

    Create routes for getChatMessages

--- Server/routes/messageRoutes.js
+++ Server/routes/messageRoutes.js
@@ -1,6 +1,7 @@
 // Server / routes / messageRoutes.js
 import express from "express";
 import {
+  getChatMessages,
   sendMessage,
   sseController,
 } from "../controllers/messageController.js";
@@ -10,5 +11,6 @@ import { protect } from "../middlewares/auth.js";
 const messageRouter = express.Router();
 messageRouter.get("/:userId", sseController);
 messageRouter.post("/send", upload.single("image"), protect, sendMessage);
+messageRouter.post("/get", protect, getChatMessages);
 
 export default messageRouter;
