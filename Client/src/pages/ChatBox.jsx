d2843c1e05825ed20bb6c46252de8cf823dd5491
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-21 20:03:04 +0530

    Add fetch and send message logic in ChatBox with state management

--- Client/src/pages/ChatBox.jsx
+++ Client/src/pages/ChatBox.jsx
@@ -1,15 +1,83 @@
-import React, { useEffect, useRef, useState } from "react";
-import { dummyMessagesData, dummyUserData } from "../assets/assets";
+// Client / src / pages / ChatBox.jsx
+import { useEffect, useRef, useState } from "react";
 import { ImageIcon, SendHorizonal } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useAuth } from "@clerk/clerk-react";
+import api, { authHeader } from "../api/axios";
+import API_ROUTES from "../api/api_route";
+import {
+  addMessage,
+  fetchMessages,
+  resetMessages,
+} from "../features/messages/messagesSlice";
+import toast from "react-hot-toast";
 
 const ChatBox = () => {
-  const messages = dummyMessagesData;
+  const { messages = [] } = useSelector((state) => state.messages);
+  const { userId } = useParams();
+  const { getToken } = useAuth();
+  const dispatch = useDispatch();
+
   const [text, setText] = useState("");
   const [image, setImage] = useState(null);
-  const [user, setUser] = useState(dummyUserData);
+  const [user, setUser] = useState(null);
   const messagesEndRef = useRef(null);
 
-  const sendMessage = async () => {};
+  const connections = useSelector((state) => state.connections.connections);
+
+  const fetchUserMessages = async () => {
+    try {
+      const token = await getToken();
+      dispatch(fetchMessages({ token, userId }));
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error?.message);
+    }
+  };
+
+  const sendMessage = async () => {
+    try {
+      if (!text && !image) return;
+
+      const token = await getToken();
+      const formData = new FormData();
+
+      formData.append("to_user_id", userId);
+      formData.append("text", text);
+      image && formData.append("image", image);
+
+      const { data } = await api.post(
+        API_ROUTES.MESSAGE.SEND_MESSAGE,
+        formData,
+        authHeader(token)
+      );
+
+      if (data.success) {
+        setText("");
+        setImage(null);
+        dispatch(addMessage(data.data));
+      } else {
+        throw new Error(data.message);
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error?.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserMessages();
+
+    return () => {
+      dispatch(resetMessages());
+    };
+  }, [userId]);
+
+  useEffect(() => {
+    if (connections.length > 0) {
+      const user = connections.find((connection) => connection._id === userId);
+      setUser(user);
+    }
+  }, [connections, userId]);
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
