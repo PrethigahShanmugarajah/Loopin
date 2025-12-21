4c3f40c2e583ce636555cefd8c737787420785ee
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-21 16:36:23 +0530

    Add follow and accept connection features in Connections page

--- Client/src/pages/Connections.jsx
+++ Client/src/pages/Connections.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+// Client / src / pages / Connections.jsx
+import { useEffect, useState } from "react";
 import {
   MessageSquare,
   User,
@@ -7,15 +8,21 @@ import {
   UserRoundPen,
 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import {
-  dummyConnectionsData as connections,
-  dummyFollowersData as followers,
-  dummyFollowingData as following,
-  dummyPendingConnectionsData as pendingConnections,
-} from "../assets/assets";
+import { useDispatch, useSelector } from "react-redux";
+import { useAuth } from "@clerk/clerk-react";
+import { fetchConnections } from "../features/connections/connectionsSlice";
+import api, { authHeader } from "../api/axios";
+import API_ROUTES from "../api/api_route";
+import toast from "react-hot-toast";
 
 const Connections = () => {
   const navigate = useNavigate();
+  const { getToken } = useAuth();
+  const dispatch = useDispatch();
+
+  const { connections, pendingConnections, followers, following } = useSelector(
+    (state) => state.connections
+  );
 
   const [currentTab, setCurrentTab] = useState("Followers");
 
@@ -25,10 +32,55 @@ const Connections = () => {
     { label: "Pending", value: pendingConnections, icon: UserRoundPen },
     { label: "Connections", value: connections, icon: UserPlus },
   ];
+
+  const handleFollow = async (userId) => {
+    try {
+      const { data } = await api.get(
+        API_ROUTES.USER.UNFOLLOW_USER,
+        { id: userId },
+        authHeader(token)
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        dispatch(fetchConnections(await getToken()));
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error?.message);
+    }
+  };
+
+  const acceptConnection = async (userId) => {
+    try {
+      const { data } = await api.get(
+        API_ROUTES.USER.ACCEPT_CONNECTION_REQUEST,
+        { id: userId },
+        authHeader(token)
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        dispatch(fetchConnections(await getToken()));
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error?.message);
+    }
+  };
+
+  useEffect(() => {
+    getToken().then((token) => {
+      dispatch(fetchConnections(token));
+    });
+  }, []);
+
   return (
     <div className="min-h-screen bg-slate-50">
       <div className="max-w-6xl mx-auto p-6">
-        {/*---------------- TITLE ----------------*/}
+        {/* -------- TITLE -------- */}
         <div className="mb-8">
           <h1 className="text-3xl font-bold text-slate-900 mb-2">
             Connections
@@ -38,20 +90,21 @@ const Connections = () => {
           </p>
         </div>
 
-        {/*---------------- COUNTS ----------------*/}
+        {/* -------- COUNTS -------- */}
         <div className="mb-8 flex flex-wrap gap-6">
           {dataArray.map((item, index) => (
             <div
               key={index}
               className="flex flex-col items-center justify-center gap-1 border h-20 w-40 border-gray-200 bg-white shadow rounded-md"
             >
-              <b>{item.value.length}</b>
+              {/* <b>{item.value.length}</b> */}
+              <b>{item.value?.length || 0}</b>
               <p className="text-slate-600">{item.label}</p>
             </div>
           ))}
         </div>
 
-        {/*---------------- TABS ----------------*/}
+        {/* -------- TABS -------- */}
         <div className="inline-flex flex-wrap items-center border border-gray-200 rounded-md p-1 bg-white shadow-sm">
           {dataArray.map((tab) => (
             <button
@@ -76,11 +129,11 @@ const Connections = () => {
           ))}
         </div>
 
-        {/*---------------- CONNECTIONS ----------------*/}
+        {/* -------- CONNECTIONS -------- */}
         <div className="flex flex-wrap gap-6 mt-6">
           {dataArray
             .find((item) => item.label === currentTab)
-            .value.map((user) => (
+            ?.value?.map((user) => (
               <div
                 key={user.id}
                 className="'w-full max-w-88 flex gap-5 p-6 bg-white shadow rounded-md"
@@ -106,16 +159,25 @@ const Connections = () => {
                         View Profile
                       </button>
                     }
+
                     {currentTab === "Following" && (
-                      <button className="w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer">
+                      <button
+                        onClick={() => handleFollow(user._id)}
+                        className="w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer"
+                      >
                         Unfollow
                       </button>
                     )}
+
                     {currentTab === "Pending" && (
-                      <button className="w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer">
+                      <button
+                        onClick={() => acceptConnection(user._id)}
+                        className="w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer"
+                      >
                         Accept
                       </button>
                     )}
+
                     {currentTab === "Connections" && (
                       <button
                         onClick={() => navigate(`/messages/${user._id}`)}
