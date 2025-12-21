cc09b227320dea90d0dc7d371e83b10d5bc2b35b
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-21 15:48:55 +0530

    Save story images and videos in media_urls

--- Server/controllers/storyController.js
+++ Server/controllers/storyController.js
@@ -11,7 +11,8 @@ export const addUserStory = async (req, res) => {
     const { userId } = req.auth();
     const { content, media_type, background_color } = req.body;
     const media = req.file;
-    let media_url = "";
+    // let media_url = "";
+    let media_urls = [];
 
     // Upload media to imagekit
     if (media_type === "image" || media_type === "video") {
@@ -20,16 +21,18 @@ export const addUserStory = async (req, res) => {
         file: fileBuffer,
         fileName: media.originalname,
       });
-      media_url = response.url;
+      // media_url = response.url;
+      media_urls.push(response.url);
     }
 
     // Create story
     const story = await Story.create({
       user: userId,
       content,
-      media_url,
+      // media_url,
       media_type,
       background_color,
+      media_urls,
     });
 
     // Schedule story deletion after 24 hours
@@ -58,7 +61,12 @@ export const getStories = async (req, res) => {
     const user = await User.findById(userId);
 
     // User connections and followings
-    const userIds = [userId, ...user.connections, ...user.following];
+    // const userIds = [userId, ...user.connections, ...user.following];
+    const userIds = [
+      userId,
+      ...(user.connections || []),
+      ...(user.following || []),
+    ];
 
     const stories = await Story.find({ user: { $in: userIds } })
       .populate("user")
