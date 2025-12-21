4c3e9daa8c44def1e6073a95972381ae00acb72d
PrethigahShanmugarajah <prethigah@gmail.com>
2025-12-21 16:11:21 +0530

    Add fetchConnections asyncThunk and update state in connectionsSlice

--- Client/src/features/connections/connectionsSlice.js
+++ Client/src/features/connections/connectionsSlice.js
@@ -1,5 +1,7 @@
 // Client / src / features / connections / connectionsSlice.js
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api, { authHeader } from "../../api/axios";
+import API_ROUTES from "../../api/api_route";
 
 const initialState = {
   connections: [],
@@ -8,10 +10,31 @@ const initialState = {
   following: [],
 };
 
+export const fetchConnections = createAsyncThunk(
+  "connections/fetchConnections",
+  async (token) => {
+    const { data } = await api.get(
+      API_ROUTES.USER.GET_USER_CONNECTIONS,
+      authHeader(token)
+    );
+    return data.success ? data : null;
+  }
+);
+
 const connectionsSlice = createSlice({
   name: "connections",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchConnections.fulfilled, (state, action) => {
+      if (action.payload) {
+        state.connections = action.payload.connections;
+        state.pendingConnections = action.payload.pendingConnections;
+        state.followers = action.payload.followers;
+        state.following = action.payload.following;
+      }
+    });
+  },
 });
 
 export default connectionsSlice.reducer;
